fb7b5af6f38f4b2fa6dab1fb830df5727aba70ff
flynndcs <flynndcs@gmail.com>
2019-01-01 21:48:24 -0600

    Add project files.

---
+++ WebApplication1/Models/Drink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class Drink
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+
+    }
+
+    public class DrinkDBContext : DbContext
+    {
+        public DbSet<Drink> Drinks { get; set; }
+    }
+}
